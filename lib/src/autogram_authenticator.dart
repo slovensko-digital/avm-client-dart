0edbf97160287893c616c50ff90005d3a8235218
Matej Hlatký <hlatky@freevision.sk>
2026-03-25 20:03:01 +0100

    Fix override

--- lib/src/autogram_authenticator.dart
+++ lib/src/autogram_authenticator.dart
@@ -1,4 +1,8 @@
+import 'dart:async';
+
 import 'package:chopper/chopper.dart';
+
+// TODO Drop AutogramAuthenticator and implement using custom DynamicMap<K, V> : MapBase<K, V> that will accept K Function()
 
 /// Sets the "X-Encryption-Key" and "Accept": "application/json" values.
 class AutogramAuthenticator extends HeadersInterceptor {
@@ -7,15 +11,18 @@ class AutogramAuthenticator extends HeadersInterceptor {
   AutogramAuthenticator(this.encryptionKeySource) : super(const {});
 
   @override
-  // TODO Fix override
-  // TODO Add tests
-  Future<Request> onRequest(Request request) async {
+  FutureOr<Response<BodyType>> intercept<BodyType>(
+    Chain<BodyType> chain,
+  ) async {
+    // Typically would not need to override and only use the map passed
+    // via the constructor; however since the encryptionKeySource
+    // is function, this needs to be evaluated dynamically
     final encryptionKey = encryptionKeySource();
-    final allHeaders = {
+    final headers = {
       "Accept": "application/json",
       "X-Encryption-Key": encryptionKey,
     };
 
-    return applyHeaders(request, allHeaders);
+    return chain.proceed(applyHeaders(chain.request, headers));
   }
 }
